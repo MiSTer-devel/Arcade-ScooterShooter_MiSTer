8f8b9a9994cea1f371b928cf6bb883a02788d937
jackyangantelope <jack@retroremake.co>
2025-12-09 21:59:37 +0800

    Update sys (#4)

--- Arcade-ScooterShooter.sv
+++ Arcade-ScooterShooter.sv
@@ -65,6 +65,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -190,6 +192,8 @@ assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 
 assign VGA_F1    = 0;
 assign VGA_SCALER= 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 assign USER_OUT  = '1;
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
